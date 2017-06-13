996cd70ce85e225e63712e9bc79239f0102e91e5
auxlepus <auxlepus@gmail.com>
2017-06-13 07:20:38 -0700

    Corrected the import statement and also updated the command comments.

--- command_example.py
+++ command_example.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-import simple_rover_sj_17
+from simple_rover_sj_17 import *
 
-# forward()     # defaults to 0.3 meters
-# forward(1.0)  # any positive number in meters
-# backward()    # defaults to 0.3 meters
-# backward(1.0) # any positive number in meters
-# right()       # defaults to 0.3 meters
-# right(1.0)    # any positive number in meters
-# left()        # defaults to 0.3 meters
-# left()        # any positive number in meters
+# stop()        # stop
+# forward()     # go forward 0.3 meters
+# forward(1.0)  # go forward some distance, any positive number in meters
+# backward()    # go backward 0.3 meters
+# backward(1.0) # go backward some distance, any positive number in meters
+# right()       # turn right 90 deg
+# right(1.0)    # turn right some angle, any positive number in deg
+# left()        # turn left 90 deg
+# left()        # turn left some angle, any positive number in deg
 # isblocked()   # returns True if front is blocked, otherwise returns False
 # temperature() # returns the temperature in Celsius
 # humidity()    # returns the humidity in %
