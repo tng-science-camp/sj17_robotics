1e43ada8c32cbf1507b327675ecf0d23f6c16785
auxlepus <auxlepus@gmail.com>
2017-06-13 06:52:30 -0700

    Added the command example file.

---
+++ command_example.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import simple_rover_sj_17
+
+# forward()     # defaults to 0.3 meters
+# forward(1.0)  # any positive number in meters
+# backward()    # defaults to 0.3 meters
+# backward(1.0) # any positive number in meters
+# right()       # defaults to 0.3 meters
+# right(1.0)    # any positive number in meters
+# left()        # defaults to 0.3 meters
+# left()        # any positive number in meters
+# isblocked()   # returns True if front is blocked, otherwise returns False
+# temperature() # returns the temperature in Celsius
+# humidity()    # returns the humidity in %
+# heading()     # returns the heading in degrees
+# zmag()        # returns the magnitude of magnetic field in the z-direction
+# arm()         # arms the lance
+# disarm()      # disarms the lance
