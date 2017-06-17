10a1469452743fd7ea9a5bc6e6cc8d53ebf3ad24
auxlepus <auxlepus@gmail.com>
2017-06-17 05:58:59 -0700

    Added an example to run as a test.

--- command_example.py
+++ command_example.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from simple_rover_sj17 import *
 
+# Following are the commands that are available:
 # stop()        # stop
 # forward()     # go forward 0.3 meters
 # forward(1.0)  # go forward some distance, any positive number in meters
@@ -9,7 +10,7 @@ from simple_rover_sj17 import *
 # right()       # turn right 90 deg
 # right(1.0)    # turn right some angle, any positive number in deg
 # left()        # turn left 90 deg
-# left()        # turn left some angle, any positive number in deg
+# left(1.0)     # turn left some angle, any positive number in deg
 # isblocked()   # returns True if front is blocked, otherwise returns False
 # temperature() # returns the temperature in Celsius
 # humidity()    # returns the humidity in %
@@ -18,3 +19,17 @@ from simple_rover_sj17 import *
 # arm()         # arms the lance
 # disarm()      # disarms the lance
 # image()       # takes an image and saves it to /var/www/html/rover_img/.
+
+
+forward()     # go forward 0.3 meters
+backward()    # go backward 0.3 meters
+right()       # turn right 90 deg
+left()        # turn left 90 deg
+isblocked()   # returns True if front is blocked, otherwise returns False
+temperature() # returns the temperature in Celsius
+humidity()    # returns the humidity in %
+heading()     # returns the heading in degrees
+zmag()        # returns the magnitude of magnetic field in the z-direction
+arm()         # arms the lance
+disarm()      # disarms the lance
+image()       # takes an image and saves it to /var/www/html/rover_img/.
